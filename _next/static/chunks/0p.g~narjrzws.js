f6cee2d82d3718d0aa862e16cf806c0798542d84
Khel <torres.michael36@gmail.com>
2026-05-30 01:35:32 +0800

    Updates

--- _next/static/chunks/0p.g~narjrzws.js
+++
@@ -1,5 +0,0 @@
-__turbopack_load_page_chunks__("/about", [
-  "static/chunks/0adq.6cs6ubvx.js",
-  "static/chunks/11li9qp2iamtf.js",
-  "static/chunks/turbopack-13v8286umx74o.js"
-])
---
+++ _next/static/chunks/0wz8~8yzweknx.js
@@ -0,0 +1,5 @@
+__turbopack_load_page_chunks__("/about", [
+  "static/chunks/08.xjly0kh8f-.js",
+  "static/chunks/0rfxwhp1qh5zy.js",
+  "static/chunks/turbopack-10j~9qwx2c5gi.js"
+])
