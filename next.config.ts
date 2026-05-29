99bdbc4a1d462fe5a64ea589ae755c18675e132c
Khel <torres.michael36@gmail.com>
2026-05-29 22:38:29 +0800

    add deployment

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',            // Enables the static export feature
+  basePath: '/torres-portfolio', // Replace with your exact GitHub repository name
+  images: {
+    unoptimized: true,         // Disables server-side image optimization
+  },
 };
 
 export default nextConfig;
