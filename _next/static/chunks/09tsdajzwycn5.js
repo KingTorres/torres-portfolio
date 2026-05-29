99209ce7c78007f4a1a2d64d099f54937d36eacc
Khel <torres.michael36@gmail.com>
2026-05-30 01:46:40 +0800

    Updates

--- _next/static/chunks/09tsdajzwycn5.js
+++ _next/static/chunks/09tsdajzwycn5.js
@@ -1,5 +1,5 @@
 __turbopack_load_page_chunks__("/_app", [
   "static/chunks/0_q8aa4ytvded.js",
-  "static/chunks/0rfxwhp1qh5zy.js",
-  "static/chunks/turbopack-0mdhae_52n5mc.js"
+  "static/chunks/0a5c1k28dt~it.js",
+  "static/chunks/turbopack-0kr5vws09ia1u.js"
 ])
--- _next/static/vnSle78T3Quce2mFl4plc/_buildManifest.js
+++ _next/static/vnSle78T3Quce2mFl4plc/_buildManifest.js
@@ -1,16 +1,16 @@
 self.__BUILD_MANIFEST = {
   "/_error": [
-    "static/chunks/0ofp4.e22xd-m.js"
+    "static/chunks/0k8p-h4bhzrn4.js"
   ],
   "/about": [
-    "static/chunks/0wz8~8yzweknx.js"
+    "static/chunks/0px1z958ht52b.js"
   ],
   "__rewrites": {
     "afterFiles": [],
     "beforeFiles": [
       {
-        "source": "/torres-portfolio//_next/:path+",
-        "destination": "/torres-portfolio/_next/:path+"
+        "source": "/your-repo-name//_next/:path+",
+        "destination": "/your-repo-name/_next/:path+"
       }
     ],
     "fallback": []
